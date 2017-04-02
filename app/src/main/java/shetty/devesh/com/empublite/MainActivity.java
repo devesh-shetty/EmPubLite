446ac22bd9090838256486f339c44b3fd2380bab
Devesh Shetty <devesh.shetty007@gmail.com>
2017-04-02 18:33:12 +0530

    Initial android proj setup

---
+++ app/src/main/java/shetty/devesh/com/empublite/MainActivity.java
@@ -0,0 +1,13 @@
+package shetty.devesh.com.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
